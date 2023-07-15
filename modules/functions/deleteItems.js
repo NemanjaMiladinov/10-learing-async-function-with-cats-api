44b13c6dca577e05ee1cb72c1db407683baa0109
Nemanja Miladinov <nemchytheking@gmail.com>
2023-07-15 07:04:12 +0200

    added delete confirmation

--- modules/functions/deleteItems.js
+++ modules/functions/deleteItems.js
@@ -1,13 +1,42 @@
 import { clearLikes } from "./likeFunctionality.js";
 
-const deleteItems = (element) => {
+const confirmDelete = async (element, modal) => {
+  const btnConfirm = document.querySelector(".button-wrapper");
+
+  function clickHandler(event) {
+    if (event.target.textContent !== "no") {
+      console.log("remove item");
+      element.remove();
+      modal.style.display = "none";
+    } else {
+      console.log("dont remove item");
+      modal.style.display = "none";
+      return;
+    }
+  }
+
+  return new Promise(() => {
+    btnConfirm.addEventListener("click", clickHandler);
+  });
+};
+
+const deleteItems = async (element) => {
+  const confirmDeleteModal = document.querySelector(".confirm-delete-modal");
+
   // element is removeFavorite icon created in likeFuntionality
   if (element) {
-    element.addEventListener("click", function (e) {
+    element.addEventListener("click", async function (e) {
       console.log("deleting items from favorite cats");
       console.log(e.target.parentElement);
       const deleteItem = e.target.parentElement;
-      deleteItem.remove();
+
+      // show modal
+      confirmDeleteModal.style.display = "block";
+      // wait for delete confirm
+      await confirmDelete(deleteItem, confirmDeleteModal);
+
+      // deleteItem.remove();
+
       // clear like state
       clearLikes();
     });
