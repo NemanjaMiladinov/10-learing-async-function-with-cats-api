054d56c8b4dc19ec9c5864e99db571fff1cb2178
Nemanja Miladinov <nemchytheking@gmail.com>
2023-07-18 14:46:01 +0200

    fixed bugs in like check / now its working properly

--- modules/functions/likeCheck.js
+++ modules/functions/likeCheck.js
@@ -1,7 +1,38 @@
 import { likeWarningSameCat } from "../modals/likeWarningSameCat.js";
-import { clearLikes, getLikedNames, likeInfo } from "./likeFunctionality.js";
-const likeCheck = () => {
+
+// import { likeInfo } from "./likeFunctionality.js";
+const likeCheck = (catName) => {
   console.log("checking is user like same cat 2 times...");
+  const favoritesCatsNames = document.querySelectorAll(".favorite-cat-name");
+
+  // Da li postoje duplikati ?
+  for (let i = 0; i < favoritesCatsNames.length; i++) {
+    /*
+      uporedi da li element vec sadrzi string
+      sa imenom koji je kreiran ...
+
+      Sa obzirom da pre apendovanja elemenata prvo ide provera,
+      u prvoj proveri nece postojati string sa isitm imenom,
+      ali ako durgi put dodamo isto ime ovaj if ce biti true ...
+    */
+
+    if (favoritesCatsNames[i].textContent.includes(catName.textContent)) {
+      console.log("Postoji duplikat u imenima mačaka.");
+      likeWarningSameCat();
+      return true;
+    }
+  }
+
+  // for (let i = 0; i < favoriteCatName.length; i++) {
+  //   console.log(favoriteCatName);
+  //   if (favoriteCatName[i].textContent.includes(getLikedNames()[i])) {
+  //     console.log("found duplicate return");
+
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
   /*
     Using index [i] variable in indexOf as a starting
     point from where elements inside index will
@@ -12,18 +43,18 @@ const likeCheck = () => {
     element exist , it would start from 0 ...
   */
 
-  for (let i = 0; i < likeInfo.likedCatsNames.length; i++) {
-    if (
-      likeInfo.likedCatsNames.indexOf(likeInfo.likedCatsNames[i], [i + 1]) >= 0
-    ) {
-      console.log("you have duplicates");
-      // ako imam duplikat obrisi taj element
-      // loop curretn state and delete element at right index
-      likeInfo.state[i].remove();
+  // for (let i = 0; i < likeInfo.likedCatsNames.length; i++) {
+  //   if (
+  //     likeInfo.likedCatsNames.indexOf(likeInfo.likedCatsNames[i], [i + 1]) >= 0
+  //   ) {
+  //     console.log("you have duplicates");
+  //     // ako imam duplikat obrisi taj element
+  //     // loop curretn state and delete element at right index
+  //     likeInfo.state[i].remove();
 
-      console.log(likeInfo.likedCatsNames[i]);
-    }
-  }
+  //     console.log(likeInfo.likedCatsNames[i]);
+  //   }
+  // }
 
   // for (let i = 0; i < getLikedNames().length; i++) {
   //   const likes = getLikedNames()[i];
